cbd923d885e6852c49fc13bd86c6ee4b1f9fdf20
Sergei Lychagin <lychagin0@gmail.com>
2021-03-19 08:50:12 +0400

    fix_Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,11 @@ public class Main {
     public static void main(String[] args) {
         BonusService service = new BonusService();
 
-        long expected = 30;
-
         // вызываем целевой метод
-        long actual = service.calculate(1000_60, true);
-        // сравниваем ожидаемый и фактический результат
-        boolean passed = expected == actual;
+        long bonus = service.calculate(1000_60, true);
 
         // выводим результат
-        System.out.println(passed);
+        System.out.println(bonus);
 
     }
 }
